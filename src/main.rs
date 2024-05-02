de061c9c91ab9108adcbbe15549c6b7d04b89300
keifufu <github@keifufu.dev>
2024-05-02 20:41:21 +0200

    draw corners

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use smithay_client_toolkit::{
   compositor::{CompositorHandler, CompositorState},
-  delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_simple,
+  delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_shm,
+  delegate_simple,
   output::{OutputHandler, OutputState},
   reexports::{
     client::{
@@ -10,17 +11,13 @@ use smithay_client_toolkit::{
         wl_buffer::{self, WlBuffer},
         wl_output::WlOutput,
         wl_region::WlRegion,
+        wl_shm::Format,
       },
       Connection, Dispatch, QueueHandle,
     },
-    protocols::wp::{
-      single_pixel_buffer::v1::client::wp_single_pixel_buffer_manager_v1::{
-        self, WpSinglePixelBufferManagerV1,
-      },
-      viewporter::client::{
-        wp_viewport::{self, WpViewport},
-        wp_viewporter::{self, WpViewporter},
-      },
+    protocols::wp::viewporter::client::{
+      wp_viewport::{self, WpViewport},
+      wp_viewporter::{self, WpViewporter},
     },
   },
   registry::{ProvidesRegistryState, RegistryState, SimpleGlobal},
@@ -29,9 +26,11 @@ use smithay_client_toolkit::{
     wlr_layer::{KeyboardInteractivity, Layer, LayerShell, LayerShellHandler, LayerSurface},
     WaylandSurface,
   },
+  shm::{raw::RawPool, Shm, ShmHandler},
 };
 
 pub const DEFAULT_ALPHA: f32 = 0.5;
+pub const DEFAULT_RADIUS: u32 = 0;
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -42,6 +41,12 @@ pub struct DimlandArgs {
     help = format!("0.0 is transparent, 1.0 is opaque, default is {DEFAULT_ALPHA}")
   )]
   pub alpha: Option<f32>,
+  #[arg(
+    short,
+    long,
+    help = format!("The radius of the opaque screen corners, default is {DEFAULT_ALPHA}")
+  )]
+  pub radius: Option<u32>,
 }
 
 fn main() {
@@ -54,9 +59,11 @@ fn main() {
 
   let compositor = CompositorState::bind(&globals, &qh).expect("no compositor :sukia:");
   let layer_shell = LayerShell::bind(&globals, &qh).expect("huh?");
+  let shm = Shm::bind(&globals, &qh).expect("wl_shm is not available");
 
   let alpha = args.alpha.unwrap_or(DEFAULT_ALPHA);
-  let mut data = DimlandData::new(compositor, &globals, &qh, layer_shell, alpha);
+  let radius = args.radius.unwrap_or(DEFAULT_RADIUS);
+  let mut data = DimlandData::new(compositor, &globals, &qh, layer_shell, alpha, radius, shm);
 
   while !data.should_exit() {
     event_queue.blocking_dispatch(&mut data).expect("sus");
@@ -68,11 +75,18 @@ pub struct DimlandData {
   registry_state: RegistryState,
   output_state: OutputState,
   layer_shell: LayerShell,
-  pixel_buffer_mgr: SimpleGlobal<WpSinglePixelBufferManagerV1, 1>,
   viewporter: SimpleGlobal<WpViewporter, 1>,
   alpha: f32,
+  radius: u32,
   views: Vec<DimlandView>,
   exit: bool,
+  shm: Shm,
+}
+
+impl ShmHandler for DimlandData {
+  fn shm_state(&mut self) -> &mut Shm {
+    &mut self.shm
+  }
 }
 
 struct DimlandView {
@@ -92,21 +106,21 @@ impl DimlandData {
     qh: &QueueHandle<Self>,
     layer_shell: LayerShell,
     alpha: f32,
+    radius: u32,
+    shm: Shm,
   ) -> Self {
     Self {
       compositor,
       registry_state: RegistryState::new(globals),
       output_state: OutputState::new(globals, qh),
       layer_shell,
-      pixel_buffer_mgr: SimpleGlobal::<WpSinglePixelBufferManagerV1, 1>::bind(globals, qh)
-        .expect("wp_single_pixel_buffer_manager_v1 not available!"),
       viewporter: SimpleGlobal::<wp_viewporter::WpViewporter, 1>::bind(globals, qh)
         .expect("wp_viewporter not available"),
-
+      radius,
       alpha,
       views: Vec::new(),
-
       exit: false,
+      shm,
     }
   }
 
@@ -146,12 +160,60 @@ impl DimlandData {
       .expect("wp_viewporter failed")
       .get_viewport(layer.wl_surface(), qh, ());
 
-    let alpha = (u32::MAX as f32 * self.alpha) as u32;
-    let buffer = self
-      .pixel_buffer_mgr
-      .get()
-      .expect("failed to get buffer")
-      .create_u32_rgba_buffer(0, 0, 0, alpha, qh, ());
+    let mut pool = RawPool::new(width as usize * height as usize * 4, &self.shm).unwrap();
+    let canvas = pool.mmap();
+
+    // TODO: corner calc is kinda wrong?
+    // see file:///stuff/screenshots/24-05-02T20-36-18.png
+    // can't be bothered right now though for it is good enough
+
+    {
+      let corner_radius = self.radius;
+
+      canvas
+        .chunks_exact_mut(4)
+        .enumerate()
+        .for_each(|(index, chunk)| {
+          let x = (index as u32) % width;
+          let y = (index as u32) / width;
+
+          let mut color = 0x00000000u32;
+          let alpha = (self.alpha * 255.0) as u32;
+          color |= alpha << 24;
+
+          if (x < corner_radius
+            && y < corner_radius
+            && (corner_radius - x).pow(2) + (corner_radius - y).pow(2) > corner_radius.pow(2))
+            || (x > width - corner_radius
+              && y < corner_radius
+              && (x - (width - corner_radius)).pow(2) + (corner_radius - y).pow(2)
+                > corner_radius.pow(2))
+            || (x < corner_radius
+              && y > height - corner_radius
+              && (corner_radius - x).pow(2) + (y - (height - corner_radius)).pow(2)
+                > corner_radius.pow(2))
+            || (x > width - corner_radius
+              && y > height - corner_radius
+              && (x - (width - corner_radius)).pow(2) + (y - (height - corner_radius)).pow(2)
+                > corner_radius.pow(2))
+          {
+            color = 0xFF000000u32;
+          }
+
+          let array: &mut [u8; 4] = chunk.try_into().unwrap();
+          *array = color.to_le_bytes();
+        });
+    }
+
+    let buffer = pool.create_buffer(
+      0,
+      width as i32,
+      height as i32,
+      width as i32 * 4,
+      Format::Argb8888,
+      (),
+      qh,
+    );
 
     DimlandView::new(qh, buffer, viewport, layer, output)
   }
@@ -292,6 +354,7 @@ delegate_output!(DimlandData);
 delegate_registry!(DimlandData);
 delegate_compositor!(DimlandData);
 delegate_simple!(DimlandData, WpViewporter, 1);
+delegate_shm!(DimlandData);
 
 impl ProvidesRegistryState for DimlandData {
   fn registry(&mut self) -> &mut RegistryState {
@@ -306,18 +369,6 @@ impl Dispatch<WpViewport, ()> for DimlandData {
     _: &mut Self,
     _: &WpViewport,
     _: wp_viewport::Event,
-    _: &(),
-    _: &Connection,
-    _: &QueueHandle<Self>,
-  ) {
-  }
-}
-
-impl Dispatch<WpSinglePixelBufferManagerV1, ()> for DimlandData {
-  fn event(
-    _: &mut Self,
-    _: &WpSinglePixelBufferManagerV1,
-    _: wp_single_pixel_buffer_manager_v1::Event,
     _: &(),
     _: &Connection,
     _: &QueueHandle<Self>,
